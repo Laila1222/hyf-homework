0adcf2bfb84e35742c762532fe654cdc524b026c
Lilla <korosililla@gmail.com>
2019-06-14 12:09:07 +0200

    added display selector

--- javascript/javascript2/week1/spiritAnimal.js
+++ javascript/javascript2/week1/spiritAnimal.js
@@ -1,21 +1,10 @@
 // Spirit animal name generator
-const spiritAnimals = ["The crying butterfly", "Amazing Ant", "Crazy Kitten", "Bobo doggu", "Hello owl", "mizi mouse", "rity rat", "munku squarrel", "miao cat", "mini mius", "liae loa"]
 
+const spiritAnimals = ["The crying butterfly", "Amazing Ant", "Crazy Kitten", "Bobo doggu", "Hello owl", "mizi mouse", "rity rat", "munku squarrel", "miao cat", "mini mius", "liae loa"]
 const btn = document.getElementById("btn");
 const inputField = document.getElementById("nameInput");
 
-
-// Spirit animal name displayed when user clicks on button. 
-btn.addEventListener('click', whenClicked);
-
-// //Spiritanimal name displayed when user hovers on input field. 
-// inputField.addEventListener("mouseover", whenClicked);
-
-// // // Spirit animal name being displayed, when user types
-// inputField.oninput = function() {whenClicked()};
-
-
-
+//Display name and spirit animal
 function whenClicked() {
     let nextAnimal = Math.floor((Math.random() * 9) + 1);
         let input = document.getElementById("nameInput").value;
@@ -26,3 +15,26 @@ function whenClicked() {
                 result.innerHTML = input + " - " + spiritAnimals[nextAnimal];
         }     
 }
+
+// Select how to display the name
+
+function displayTypes() {
+        let options = document.getElementsByName("options");
+        if (options[0].checked) {
+                btn.addEventListener('click', whenClicked); 
+                
+        }
+
+        else if (options[1].checked) {
+                inputField.addEventListener("mouseover", whenClicked);
+                
+        }
+
+        else if (options[2].checked) {
+                inputField.oninput = function() {whenClicked()};
+                
+        }
+}
+
+
+
