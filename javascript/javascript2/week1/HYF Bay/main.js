b1b996bcacb07b1988e87ddfcb7aa342f2e7cf77
Lilla <korosililla@gmail.com>
2019-06-15 11:28:49 +0200

    created li for countries

--- javascript/javascript2/week1/HYF Bay/main.js
+++ javascript/javascript2/week1/HYF Bay/main.js
@@ -2,77 +2,26 @@ console.log('Script loaded');
 
 const products = getAvailableProducts();
 renderProducts(products);
-// for (let i = 0; i < products.length; i++) {
-//     console.log(products[i].name);
-// }
-// for (let items of products) {
-//     console.log(items.shipsTo[0]);
-// }
-// console.log(products.shipsTo)
 
 function renderProducts(array) {
-    // Create main Ul and Li for products
     
-    
-    
-    // const childUl = document.querySelector(".childUl");
-    // console.log(childUl);
-    
-
     for (let productItem of array) {
-        
-        const mainUlOfProducts = document.querySelector(".products ul");
-        // const mainLiOfProducts = document.createElement("li");
-        // const childUl = document.createElement("ul");
-        // mainUlOfProducts.appendChild(mainLiOfProducts);
-        // mainLiOfProducts.appendChild(childUl);
-        const mainLi = createElement("li", "", mainUlOfProducts, "", "mainLi");
-        
-        const childUl = createElement("ul", "", mainLi, "", "childUl");
-        console.log(childUl);
-
-        
-        // const childUl = createElement("ul", "", mainLi, "", "childUl");
-
+    // Create main Ul and Li for products
+    const mainUlOfProducts = document.querySelector(".products ul");
+    const mainLi = createElement("li", "", mainUlOfProducts, "", "mainLi");
+    const childUl = createElement("ul", "", mainLi, "", "childUl");
+ 
     //Add list with product information to childUl
     createElement("li", productItem.name, childUl, "", "");
     createElement("li", productItem.price, childUl, "", "");
     createElement("li", productItem.rating, childUl, "", "");
-    createElement("li", "Ships to", childUl, "", "shipsTo");
+    const shipToLi = createElement("li", "Ships to", childUl, "", "shipsTo");
 
-    //create ul for shipto
-    // const shipsToLine = document.getElementsByClassName("shipsTo");
-    // console.log(shipsToLine);
-    // const shipsUl = document.createElement("ul");
-    // shipsToLine.appendChild(shipsUl); 
-
-    
+    //create ul and li for shipto countries
+    const shipToUl = createElement("ul", "", shipToLi, "", "");
+    const countries = createElement("li", productItem.shipsTo, shipToUl, "", "countries");
     }
-    
-
-    //loop through ships to
-    // for (let countries of products) {
-    //     console.log(countries.shipsTo);
-        
-    //     createLi(countries.shipsTo, shipsUl, "", "");
-    // }
-    
-    
 }
-
-//Create ul for Shipto ----> something is off here :/ - still working on it! 
-// function createUltoShip() {
-     
-    
-//     for (let items of products) {
-//         console.log(items.shipsTo)
-//         let countries = document.createElement("li");
-//         countries.innerHTML = items.shipsTo;
-//         shipsUl.appendChild(countries);
-//         console.log(items.shipsTo) 
-//     }  
-// }
-//Gives the countries to shipTo
 
 function createElement(tag, value, parent, id, className) {
     newElement = document.createElement(tag);
@@ -82,4 +31,3 @@ function createElement(tag, value, parent, id, className) {
     parent.appendChild(newElement);
     return newElement;
 };
-// createUltoShip();
