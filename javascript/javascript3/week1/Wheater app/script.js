a4fec9ea8516f27a31da29e06faac631f558c9e2
Lilla <korosililla@gmail.com>
2019-07-03 16:23:20 +0200

    changing background image

--- javascript/javascript3/week1/Wheater app/script.js
+++ javascript/javascript3/week1/Wheater app/script.js
@@ -3,12 +3,13 @@
 // .then(json => console.log(json.main.temp))
 
 
-const inputField = document.querySelector('.location input');
-const button = document.querySelector('button');
+const inputField = document.querySelector('#input-location');
+const button = document.querySelector('#input-location-button');
 // const temperature = document.querySelector('#temperature');
 const humidity = document.querySelector('#humidity');
 const errorMessage = document.querySelector('#errorMessage');
 const currentLocationButton = document.querySelector('#current-location');
+const background = document.querySelector('body');
 
 //Event listeners
 //Get weather data by input
@@ -54,6 +55,7 @@ function getWeatherData(url) {
         .then(json => {
            if (!json.message) {
             document.querySelector('#city-name').innerHTML = `Location: ${json.name}`;
+            document.querySelector('h3').innerHTML = json.weather[0].description;
             document.querySelector('#temperature').innerHTML = `Temperature: ${json.main.temp}°C`;
             document.querySelector('#humidity').innerHTML = `Humidity: ${json.main.humidity} hPa.`;
             document.querySelector('#min-temp').innerHTML = `Minimum-temperature: ${json.main.temp_min} °C.`;
@@ -70,11 +72,12 @@ function getWeatherData(url) {
                 const displayTime = `${hours}:${minutes}`;
                 return displayTime;
             }
-            const latitude = json.coord.lat;
-            const longitude = json.coord.lon;
+            
 
             initMap(json.coord.lat, json.coord.lon);
 
+            const weatherCode = json.weather[0].id;
+            getWeatherCondition(weatherCode);
 
            } else {
             errorMessage.innerHTML = json.message;
@@ -107,9 +110,32 @@ function initMap(coordLat, coordLng) {
         zoom: 12,
       center: location
     });
+    mapDiv.style.display = "block"
     console.log(location)
     
   }
 
-//Save location
+//Change background picture according to cloudiness
 
+
+//wheater conditions
+function getWeatherCondition (weatherCondition) {
+    if (weatherCondition <= 232 && weatherCondition >= 200) {
+
+        background.style.backgroundImage = 'url(images/storm.jpg)'
+    } else if (weatherCondition <= 321 && weatherCondition >= 300) {
+        background.style.backgroundImage = 'url(images/drizzle.jpg)'
+    } else if (weatherCondition <= 531 && weatherCondition >= 500){
+        background.style.backgroundImage = 'url(images/rainy.jpg)' 
+    } else if (weatherCondition <= 622 && weatherCondition >= 600) {
+        background.style.backgroundImage = 'url(images/snow.jpg)'
+    }else if (weatherCondition <= 781 && weatherCondition >= 701) {
+        background.style.backgroundImage = 'url(images/mist.jpg)'
+    }else if (weatherCondition = 800) {
+        background.style.backgroundImage = 'url(images/sunny.jpg)'
+    }else if (weatherCondition <= 804 && weatherCondition >= 800) {
+        background.style.backgroundImage = 'url(images/cloudandsun.jpg)'
+    } else {
+        background.style.backgroundImage = 'url(images/error.jpg)'
+    }
+}
